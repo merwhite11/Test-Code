e2a147702d2d54fb23db83d48761f42e0b19cb5e
merwhite11 <meredith.white91@gmail.com>
2024-04-26 09:02:05 -0700

    Alternative solution not working, commented out

--- qrcode-2/server.js
+++ qrcode-2/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
+// const qrcode = require('qrcode');
+// const fs = require('fs');
 
 // Serve the HTML file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+
+  //TRYING TO DO IT IN ONE GET REQ
+  // const link = "https://openlibrary.org/works/OL17860744W/A_Court_of_Mist_and_Fury";
+
+//   qrcode.toFile('./qrcode.png', link, async (err) => {
+//     if (err) {
+//       console.log('Error generating QR code:', err)
+//       return res.status(500).send('Error generating QR code')
+//     }
+
+//     res.setHeader('Content-Type', 'text/html');
+//     fs.createReadStream(__dirname + '/index.html').pipe(res)
+  // })
 });
 
 // Serve the generated QR code image
-app.get('/qrcode.png', (req, res) => {
+app.get('/qrcode', (req, res) => {
   res.sendFile(__dirname + '/qrcode.png');
 });
 
